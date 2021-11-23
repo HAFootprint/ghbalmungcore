f3055d253427a0c6a9b26fea2163a0a06db726e7
Javier Bolaños Carapia <javier.bc121086@gmail.com>
2021-11-22 23:13:51 -0600

    Initial Commit

---
+++ ghbalmungcore/ghbalmungcore.h
@@ -0,0 +1,18 @@
+//
+//  ghbalmungcore.h
+//  ghbalmungcore
+//
+//  Created by Javier Carapia on 22/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ghbalmungcore.
+FOUNDATION_EXPORT double ghbalmungcoreVersionNumber;
+
+//! Project version string for ghbalmungcore.
+FOUNDATION_EXPORT const unsigned char ghbalmungcoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ghbalmungcore/PublicHeader.h>
+
+
