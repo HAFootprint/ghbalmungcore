f3185de966bc79aae1c7dd490a5f6c35a6a4c1dc
Javier Bolaños Carapia <javier.bc121086@gmail.com>
2021-12-17 18:00:27 -0600

    Se quita try de la funcion rx

--- Sources/ghbalmungcore/implement/GHBalmungBaseRxExtension.swift
+++ Sources/ghbalmungcore/implement/GHBalmungBaseRxExtension.swift
@@ -10,8 +10,8 @@ import Combine
 
 extension GHBalmungBase {
     @available(iOS 13.0, *)
-    public func doInRxVackground(metadata: GHMetadataModel, method: GHRestType) throws -> AnyPublisher<Any, URLSession.DataTaskPublisher.Failure>? {
-        return try self.coreServiceDelegate?.submitRequest(
+    public func doInRxVackground(metadata: GHMetadataModel, method: GHRestType) -> AnyPublisher<Any, URLSession.DataTaskPublisher.Failure>? {
+        return self.coreServiceDelegate?.submitRequest(
             bundle: self.bundle,
             metadata: metadata,
             restMethod: method
